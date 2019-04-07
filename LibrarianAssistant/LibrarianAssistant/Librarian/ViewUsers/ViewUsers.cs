1a238a5a2402ff82b9290f4db772d0815f791cff
Osman <odbakari@gmail.com>
2019-04-07 15:03:05 -0700

    ReAdded ViewUsersUI

---
+++ LibrarianAssistant/LibrarianAssistant/Librarian/ViewUsers/ViewUsers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibrarianAssistant.Librarian
+{
+    public partial class ViewUsers : Form
+    {
+        public ViewUsers()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewUsers_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ViewUsers
+            // 
+            this.ClientSize = new System.Drawing.Size(980, 532);
+            this.Name = "ViewUsers";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
