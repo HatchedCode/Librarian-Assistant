e3db7f28570937137a55f07ea0b58e2e7a02e998
Osman <odbakari@gmail.com>
2019-04-07 13:29:27 -0700

    Took some of the files from AdminUI and transferred it to the the project I am working on.

---
+++ LibrarianAssistant/LibrarianAssistant/Admin/Main Screen/Form1.cs
@@ -0,0 +1,48 @@
+﻿
+namespace AdminUI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1097, 606);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
