2ee95ae0918ede9a569326e4e53231205e8cb0b1
Osman <odbakari@gmail.com>
2019-04-10 09:09:43 -0700

    Did some re-arranging with some files.

---
+++ LibrarianAssistant/LibrarianAssistant/Librarian/CreateLibrarian/CreateLibrarian.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace LibrarianAssistant
+{
+    using ApplicationEngine;
+    using ApplicationEngine.UserTypes;
+
+
+    public partial class CreateLibrarian : Form
+    {
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "AzjbyoyIlfvOFVzasWvFtWd9qUquxgEk2RNIWfXW",
+            BasePath = "https://libraryassistantv1.firebaseio.com/"
+        };
+
+        IFirebaseClient client;
+
+
+
+        public CreateLibrarian()
+        {
+            InitializeComponent();
+            this.FirstNameText.Text = "First Name";
+            this.LastNameText.Text = "Last Name";
+            this.IDText.Text = "ID";
+            this.PasswordText.Text = "Enter Password";
+            this.RePasswordText.Text = "ReEnter Password";
+            this.Back.Text = "Go Back";
+            this.Submit.Text = "Submit";
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateLibrarian_Load(object sender, EventArgs e)
+        {
+
+            client = new FireSharp.FirebaseClient(config);
+
+          
+        }
+
+        private void FirstNameText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+        }
+
+            private async void Submit_Click(object sender, EventArgs e)
+        {
+            var data = new ApplicationEngine.UserTypes.Librarian
+            {
+                FirstName = FirstName.Text,
+                LastName = LastName.Text,
+                ID = ID.Text,
+                Password = Password.Text
+            };
+
+            SetResponse response = await client.SetTaskAsync(ID.Text, data);
+            ApplicationEngine.UserTypes.Librarian results = response.ResultAs<ApplicationEngine.UserTypes.Librarian>();
+ 
+        }
+
+        private void ID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LibrarianAssistant/LibrarianAssistant/Librarian/Edit Books/EditBook.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ApplicationEngine.Book;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace LibrarianAssistant
+{
+    public partial class EditBook : Form
+    {
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "AzjbyoyIlfvOFVzasWvFtWd9qUquxgEk2RNIWfXW",
+            BasePath = "https://libraryassistantv1.firebaseio.com/"
+        };
+
+        IFirebaseClient client;
+
+        public EditBook()
+        {
+            InitializeComponent();
+            this.ISBNtext.Text = "ISBN";
+            this.TitleText.Text = "Title";
+            this.AuthorText.Text = "Author";
+            this.DateText.Text = "Date Released";
+            this.GenreText.Text = "Genre";
+            this.AvailableText.Text = "Number Available";
+            this.CHText.Text = "CurrentHolders";
+            this.button1.Text = "Submit Changes";
+            this.Back.Text = "previouse Page";
+
+            client = new FireSharp.FirebaseClient(config);
+
+        }
+
+        public void EditBook_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ISBNtext_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ISBN_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void EditBook_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            var data = new Book
+            {
+                Title = Title.Text,
+                Author = Author.Text,
+                ISBN = Convert.ToInt64(ISBN.Text),
+                Date = Date.Text,
+                Genre = Genre.Text,
+                Available = Convert.ToInt32(Available.Text)
+
+            };
+            
+            /*
+            Librarian lib= new Librarian
+            {
+                FirstName="bob",
+                LastName="ross",
+                ID="2324232",
+                Password
+
+            }
+            */
+            SetResponse response = await client.SetTaskAsync(ISBN.Text, data);
+            Book results = response.ResultAs<Book>();
+        }
+    }
+}
